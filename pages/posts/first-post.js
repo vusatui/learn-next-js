8f19391174b1bff5d7391bb4bf297d7620f2b7c1
Anton Vusatyi <Anton_Vusatyi@vfc.com>
2023-02-11 21:07:30 +0300

    Assets, Metadata and CSS

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,26 @@
+import Head from 'next/head';
+import Script from 'next/script';
 import Link from 'next/link';
 
+import Layout from "../../components/Layout";
+
 export default function FirstPost() {
+    const onScriptLoad = () => console.log(`script loaded correctly, window.FB has been populated`);
+
     return (
-        <>
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <Script
+                src="https://connect.facebook.net/en_US/sdk.js"
+                strategy="lazyOnload"
+                onLoad={onScriptLoad}
+            />
             <h1>First Post</h1>
             <h2>
                 <Link href="/">Back to home</Link>
             </h2>
-        </>
+        </Layout>
     );
 };
